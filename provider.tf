d98d57b14861eac96cc0ff9e69582015429b4b00
djoo <glimpsovstar@gmail.com>
2025-03-10 22:15:57 +1100

    Update provider.tf
    cluster_auth_token is No Longer Available in AWS EKS Module v20+

--- provider.tf
+++ provider.tf
@@ -23,9 +23,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  token                  = module.eks.cluster_auth_token
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
